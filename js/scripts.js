c0e2d1af0ac8be29876412bfd4df566b6ebcdefc
Manuela Malasaña <Xibanya@users.noreply.github.com>
2017-04-09 14:42:22 -0500

    it actually works, kind of

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 var client_id = '248ec9c765e3313b26b50b9d6cdb7f6d12c47f8ae10661d138c6d02a17d9c6a4'
-var domain = 'https://mastodon.xyz';
+var domain = 'mastodon.xyz';
 var redirect = 'https://xibanya.github.io/mastodata/index.html';
 
 function authenticate() {
-	authURL = domain + '/oauth/authorize?response_type=code&client_id='+client_id+'&redirect_uri='+redirect;
+	authURL = "https://" + domain + '/oauth/authorize?response_type=code&client_id='+client_id+'&redirect_uri='+redirect;
 	console.log(authURL);
 	document.getElementById("authlink").innerHTML = "<a href='"+authURL+"'>hi</a>";
 	return authURL;
@@ -23,6 +23,91 @@ function findGetParameter(parameterName) {
 }
 
 	var token = findGetParameter('code');
-	console.log('Token: ' + token);
+	//console.log('Token: ' + token);
 	
-	//todo, if token is null load setup form, if not proceed to load charts
+	if (token != null) {
+	    var xmlhttp = new XMLHttpRequest();
+	    xmlhttp.onreadystatechange = function () {
+	        if (this.readyState == 4 && this.status == 200) {
+	            
+	            var response = [];
+                //get every single domain in the response
+	            for (var x in JSON.parse(this.responseText)) {
+
+	                var temp = JSON.parse(this.responseText)[x].acct; //this is the full account name
+	                tempArr = [];
+	                tempArr = temp.split("@");
+	                if (tempArr[1] == null) {
+	                    response.push(domain);
+	                }
+	                else {
+	                    response.push(tempArr[1]);
+	                }
+	            }
+
+                //put all the unique domains into an array
+	            var allDomains = [];
+	            for (var n in response) {
+	                if (!allDomains.includes(response[n])) {
+	                    allDomains.push(response[n]);
+	                }
+	            }
+	            
+	            //count frequency of domains
+	            var domainFreq = {};
+	            for (var i = 0; i < response.length; i++) {
+	                var num = response[i];
+	                domainFreq[num] = domainFreq[num] ? domainFreq[num] + 1 : 1;
+	            }
+
+	            var counts = [];
+	            for (var i in allDomains) {
+	                console.log(allDomains[i] + ": " + domainFreq[allDomains[i]]);
+	                counts.push(domainFreq[allDomains[i]]);
+	            }
+
+	            var pieData = {
+	                labels: allDomains,
+	                datasets: [
+                        {
+                            data: counts,
+                            backgroundColor: [
+                                "#FF6384",
+                                "#36A2EB",
+                                "#FFCE56"
+                            ],
+                            hoverBackgroundColor: [
+                                "#FF6384",
+                                "#36A2EB",
+                                "#FFCE56"
+                            ]
+                        }]
+	            };
+
+	            var myChart = new Chart(ctx, {
+	                type: 'pie',
+	                responsive: true,
+	                data: pieData,
+	                options: {
+	                    title: {
+	                        display: true,
+	                        text: 'A Title',
+	                        position: 'bottom',
+	                        fullWidth: true
+	                    }
+	                }
+	            });
+	            
+
+	        }
+	    }
+	    xmlhttp.open("GET", "https://mastodon.xyz/api/v1/accounts/7775/followers", true);
+	    xmlhttp.setRequestHeader("Authorization", "Bearer " + token);
+	    xmlhttp.send();
+
+
+
+	}
+//todo, if token is null load setup form, if not proceed to load charts
+
+	
