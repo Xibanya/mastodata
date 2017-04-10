f4a1e6072c1cd16b78cf8df4051040db975fdcac
Manuela Malasaña <Xibanya@users.noreply.github.com>
2017-04-10 18:21:02 -0500

    oauth should now work!

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,25 @@ var client_id = '248ec9c765e3313b26b50b9d6cdb7f6d12c47f8ae10661d138c6d02a17d9c6a
 var domain = 'mastodon.xyz';
 var redirect = 'https://xibanya.github.io/mastodata/index.html';
 
-function authenticate() {
-	authURL = "https://" + domain + '/oauth/authorize?response_type=code&client_id='+client_id+'&redirect_uri='+redirect;
-	console.log(authURL);
-	document.getElementById("authlink").innerHTML = "<a href='"+authURL+"'>hi</a>";
-	return authURL;
-};
+var account;
+	//var token = 'cd5c2fc8e99c48f38ec584672fb3daeb7580414abf40fe707f7508854f27955f'
+	//var userID = '7775';
+var token = localStorage.getItem("mastodata-token");
+
+
+	if(typeof token == 'string') {
+		//TODO: remove when done debugging
+		console.log("Already have token: " + token);
+	}
+	else
+	{
+		authenticate();
+	}
+		function authenticate() {
+		authURL = "https://" + domain + '/oauth/authorize?response_type=code&client_id=' + client_id +'&redirect_uri=' + redirect;
+		console.log(authURL);
+		document.getElementById("intro").innerHTML = "<a href='"+authURL+"'>Click to authenticate</a>";
+		};
 
 function findGetParameter(parameterName) {
     var result = null,
@@ -22,13 +35,58 @@ function findGetParameter(parameterName) {
     return result;
 }
 
-	var reachedEnd = false;
-	var response = [];
-	var allDomains = [];
-	var token = findGetParameter('code');
-	//console.log('Token: ' + token);
+var code = findGetParameter('code');
+if (code != null && typeof token != 'string') 
+{
+	var data = new FormData();
+	data.append("grant_type", "authorization_code");
+	data.append("client_id", client_id);
+	data.append("client_secret", "08b9e26a6fb1c22aedf430ab0b8f7a4e0ce1b7c508b24e7f6c8510294f496f89");
+	data.append("redirect_uri", "https://xibanya.github.io/mastodata/index.html");
+	data.append("code", code);
+
+	var getToken = new XMLHttpRequest();
+	getToken.onreadystatechange = function () {
+		if (getToken.readyState == XMLHttpRequest.DONE && getToken.status == 200) {
+			token = JSON.parse(this.responseText).access_token;
+			localStorage.setItem("mastodata-token", token);
+			console.log("Token: " + token);
+		}
+	}
+		
+	getToken.open("POST", "https://mastodon.xyz/oauth/token", true);
+	getToken.send(data);
+	console.log("Sent POST to get token");
 	
-	if (token != null) {
+}
+
+if(typeof token == 'string')
+{
+	GetUser();
+}
+
+	function GetUser() {
+		var xmlhttp = new XMLHttpRequest();
+		xmlhttp.onreadystatechange = function () {
+	    if (this.readyState == 4 && this.status == 200) {
+				userID = JSON.parse(this.responseText).id;
+				account = JSON.parse(this.responseText).username;
+				console.log("userID: " + userID + ", account: " + account);
+				document.getElementById("intro").innerHTML = "<p>Hello " + account + "!</p>";	
+
+				GetFollowers();
+			}
+		}
+		
+		xmlhttp.open("GET", "https://mastodon.xyz/api/v1/accounts/verify_credentials", true);
+	    xmlhttp.setRequestHeader("Authorization", "Bearer " + token);
+	    xmlhttp.send();		
+	}
+	
+	function GetFollowers() {
+		var reachedEnd = false;
+		var response = [];
+		var allDomains = [];
 	    var xmlhttp = new XMLHttpRequest();
 	    xmlhttp.onreadystatechange = function () {
 	        if (this.readyState == 4 && this.status == 200) {
@@ -57,31 +115,36 @@ function findGetParameter(parameterName) {
 								xmlhttp.send();
 						}
 						else {
-							PrepChartData();
+							PrepChartData(response, allDomains);
 						}
 			}
 		}
+		
+		xmlhttp.open("GET", "https://mastodon.xyz/api/v1/accounts/" + userID + "/followers", true);
+	    xmlhttp.setRequestHeader("Authorization", "Bearer " + token);
+	    xmlhttp.send();
+	}
 
-		function PrepChartData() {
+		function PrepChartData(rawResponse, domains) {
 			//put all the unique domains into an array
-			for (var n in response) {
-	            if (!allDomains.includes(response[n])) {
-	                allDomains.push(response[n]);
+			for (var n in rawResponse) {
+	            if (!domains.includes(rawResponse[n])) {
+	                domains.push(rawResponse[n]);
 	            }
 	        }
 				            //count frequency of domains
 	            var domainFreq = {};
-	            for (var i = 0; i < response.length; i++) {
-	                var num = response[i];
+	            for (var i = 0; i < rawResponse.length; i++) {
+	                var num = rawResponse[i];
 	                domainFreq[num] = domainFreq[num] ? domainFreq[num] + 1 : 1;
 	            }
 				var counts = [];
-	            for (var i in allDomains) {
-	                console.log(allDomains[i] + ": " + domainFreq[allDomains[i]]);
-	                counts.push(domainFreq[allDomains[i]]);
+	            for (var i in domains) {
+	                console.log(domains[i] + ": " + domainFreq[domains[i]]);
+	                counts.push(domainFreq[domains[i]]);
 	            }
 
-				DrawChart(allDomains, counts);				
+				DrawChart(domains, counts);				
 		}
 		
 		function DrawChart(domains, dataset) {
@@ -112,7 +175,7 @@ function findGetParameter(parameterName) {
 								"chocolate",
 								"coral",
 								"cornflowerblue",
-								"consilk"
+								"cornsilk"
 								
                             ],
                             hoverBackgroundColor: [
@@ -136,7 +199,7 @@ function findGetParameter(parameterName) {
 								"chocolate",
 								"coral",
 								"cornflowerblue",
-								"consilk"								
+								"cornsilk"								
                             ]
                         }]
 	            };
@@ -148,17 +211,14 @@ function findGetParameter(parameterName) {
 	                options: {
 	                    title: {
 	                        display: true,
-	                        text: 'Followers',
+	                        text: account + "'s Followers",
 	                        position: 'bottom',
 	                        fullWidth: true
 	                    }
 	                }
 	            });
 		}
-	            
-	    xmlhttp.open("GET", "https://mastodon.xyz/api/v1/accounts/7775/followers", true);
-	    xmlhttp.setRequestHeader("Authorization", "Bearer " + token);
-	    xmlhttp.send();
+	           
 
-	}
+	
 //todo, if token is null load setup form, if not proceed to load charts
