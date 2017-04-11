f728dd81cf4591656518f4b4253dac10ad2886e2
Xibanya <manuelamalasanya@gmail.com>
2017-04-11 18:20:46 -0500

    adding angular.js

--- Mastodata/bin/Debug/AppX/js/common.js
+++ Mastodata/bin/Debug/AppX/js/common.js
@@ -1,5 +1,5 @@
-angular.module('mastodata',[])
-.controller('mainController', function($scope) {
+var app = angular.module("mastodata",[]);
+app.controller("mainController", function($scope) {
 	$scope.domains = [];
 var domainInput = "<div class='input-group'><span class='input-group-addon'>Instance</span><input type='text' class='form-control' id='instanceInput'></div><button type='button' class='btn btn-primary'>Go!</button>";
 
@@ -11,26 +11,37 @@ var storedDomainsCount;
 	$scope.sortReverse = false;
 	$scope.searchInstance = '';
 	
-	
+    //FOR DEBUG ONLY
+
+    //OK THANKS
 
 	$scope.initialize = function() 
 	{
 	//do we have a saved domain?
-	//TODO: multidimensional saved domains
-		storedDomainsCount = localStorage.getItem("s")
-		if (typeof localStorage.getItem("mastodata-domain") != 'string')
+	    
+        //how many domains do we have saved?
+	   
+
+
+	    if (typeof localStorage.getItem("mastodata-domain0") != 'string' || typeof localStorage.getItem("storedCount") != 'string' || localStorage.getItem("storedCount") == "0" || parseInt(localStorage.getItem("storedCount")) == "Nan")
 		{
 			//we don't have a domain saved
 			//create input form
-			document.getElementById('intro').innerHTML = domainInput;
+	        document.getElementById('intro').innerHTML = domainInput;
 		}
 		else
 		{
-			//we do have a domain saved
+	        //we do have a domain saved
+	        //how many?
+	        storedDomainsCount = parseInt(localStorage.getItem("storedCount"));
+	        for (i = 0; i < storedDomainsCount; i++)
+	        {
+	            $scope.domains.push(JSON.parse(localStorage.getItem("mastodata-domain" + i)));
+	            console.log($scope.domains);
+	        }
+
 			//ask the user if they would like to continue to use this domain or use a new one
-			$scope.domains = JSON.parse(localStorage.getItem("mastodata-domain"));
-			console.log($scope.domains);
-			document.getElementById('intro').innerHTML = domanList;
+			document.getElementById('intro').innerHTML = domainList;
 			//for each stored domain, list them then ask user which they want to use
 		}
 	
